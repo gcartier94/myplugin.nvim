e4b6a77f24419c0e3a5084cf19df32afa3447c41
Guilherme Cartier <gcartier@kyndryl.com>
2024-01-11 17:07:49 -0300

    updates
    Signed-off-by: Guilherme Cartier <gcartier@kyndryl.com>

--- lua/myplug/init.lua
+++ lua/myplug/init.lua
@@ -1,16 +1,23 @@
 local M = {} -- M stands for module, a naming convention
 
-function M.setup(opts)
-    opts = opts or {}
-
-    vim.keymap.set("n", "<Leader>h", function()
-        if opts.name then
-            print("hello, " .. opts.name)
-        else
-            print("hello")
-        end
-
-    end)
+function M.setup()
+  local lines = {"Hello"}
+  local width = 20
+  local height = #lines
+  local row = math.floor((vim.o.lines - height) / 2)
+  local col = math.floor((vim.o.columns - width) / 2)
+  local opts = {
+    relative = "editor",
+    row = row,
+    col = col,
+    width = width,
+    height = height,
+    style = "minimal"
+  }
+  local buf = vim.api.nvim_create_buf(false, true)
+  local win = vim.api.nvim_open_win(buf, true, opts)
+  vim.api.nvim_buf_set_lines(buf, 0, -1, false, lines)
+  vim.api.nvim_buf_set_option(buf, "modifiable", false)
 end
 
 return M
